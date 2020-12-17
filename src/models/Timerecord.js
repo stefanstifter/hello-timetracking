d63982e1fcc2364ec6d07b8ef94c7be2da17e4af
stefanstifter <stefan.stifter@saswebdev.com>
2020-12-17 20:19:30 +0100

    install moment.js

--- src/models/Timerecord.js
+++ src/models/Timerecord.js
@@ -1,7 +1,10 @@
 import { v4 as uuid } from 'uuid';
+import moment from 'moment';
 
 class Timerecord {
     constructor(timerecordData) {
+        console.log('hey');
+        console.log(moment().format());
         this.id = uuid(),
         this.project = timerecordData.project;
         this.hours = timerecordData.hours;
