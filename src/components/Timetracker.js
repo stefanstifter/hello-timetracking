0d2e2e315ebb09a43a85edd03a79d0863286132d
stefanstifter <stefan.stifter@saswebdev.com>
2020-12-17 17:21:11 +0100

    extract initial data into `initial-timerecords` file. experiment with import of react-native-get-random-values and uuid

--- src/components/Timetracker.js
+++ src/components/Timetracker.js
@@ -2,21 +2,16 @@ import React, { useState } from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
 import TimeRecordCreate from './TimeRecordCreate';
 import RecordItem from './RecordItem';
+import { timerecords } from "../initial-timerecords";
 import { v4 as uuid } from 'uuid';
 
 const Timetracker = () => {
-    const [ records, setRecords ] = useState([
-        { id: uuid(), text: 'Project A: 17h' },
-        { id: uuid(), text: 'Project B: 23h' },
-        { id: uuid(), text: 'Project C: 12h' },
-    ]);
-
+    const [ records, setRecords ] = useState(timerecords);
     const createTimeRecord = (text) => {
         setRecords(prev => {
             return [ ...prev, {id: uuid(), text} ];
         });
     }
-
     const deleteTimeRecord = (id) => {
         setRecords(prev => prev.filter(item => item.id !== id));
     };
---
+++ src/initial-timerecords.js
@@ -0,0 +1,8 @@
+import 'react-native-get-random-values';
+import { v4 as uuid } from 'uuid';
+
+export const timerecords = [
+    { id: uuid(), text: 'Project D: 17h' },
+    { id: uuid(), text: 'Project E: 23h' },
+    { id: uuid(), text: 'Project G: 12h' },
+];
